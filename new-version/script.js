bed69b8d1fc6aae83650d6485816279aa8c4c07b
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-08-31 19:30:08 -0300

    colocando os pokemons na div com imagem e nome

--- new-version/script.js
+++ new-version/script.js
@@ -1,9 +1,26 @@
-const div = document.getElementById('res')
+const div = document.getElementById('imgs')
+const res = document.getElementById('res')
+const p = document.createElement('p')
+const nomePokes = ['','Bulbasaur','Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise']
+const types = []
+
 
 for(let c = 1; c < 10; c++){
+    const a = document.createElement('a')
     const li = document.createElement('li')
     const img = document.createElement('img')
+    a.appendChild(img)
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${c}.png`
-    li.appendChild(img)
+    img.id = nomePokes[c]
+    img.onclick = function (){
+        res.innerHTML = ''
+        const imgtemp = document.createElement('img')
+        imgtemp.src = img.src
+        imgtemp.style.width = '50%'
+        p.innerHTML = img.id
+        res.appendChild(p)
+        res.appendChild(imgtemp)
+    }
+    li.appendChild(a)
     div.appendChild(li)
 }
