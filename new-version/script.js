7a9d07118c4d8c522565e0f5f8c0d4c0d768b22b
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-09-01 10:03:08 -0300

    colocando os tipos

--- new-version/script.js
+++ new-version/script.js
@@ -1,8 +1,43 @@
 const div = document.getElementById('imgs')
 const res = document.getElementById('res')
 const p = document.createElement('p')
+const span = document.createElement('span')
+const span2 = document.createElement('span')
 const nomePokes = ['','Bulbasaur','Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise']
-const types = []
+const types = [
+    {},
+    {
+        tipo1: 'Grass',
+        tipo2: 'Poison'
+    },
+    {
+        tipo1: 'Grass',
+        tipo2: 'Poison'
+    },
+    {
+        tipo1: 'Grass',
+        tipo2: 'Poison'
+    },
+    {
+        tipo1: 'fire',
+    },
+    {
+        tipo1: 'fire',
+    },
+    {
+        tipo1: 'fire',
+        tipo2: 'Flying'
+    },
+    {
+        tipo1: 'walter'
+    },
+    {
+        tipo1: 'walter'
+    },
+    {
+        tipo1: 'walter'
+    },
+]
 
 
 for(let c = 1; c < 10; c++){
@@ -13,13 +48,49 @@ for(let c = 1; c < 10; c++){
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${c}.png`
     img.id = nomePokes[c]
     img.onclick = function (){
+        span2.innerHTML = ''
         res.innerHTML = ''
+        span.innerHTML = types[c].tipo1
+        res.backgroundColor = 'gray'
         const imgtemp = document.createElement('img')
         imgtemp.src = img.src
         imgtemp.style.width = '50%'
         p.innerHTML = img.id
+        
+        
+        if(img.id == 'Bulbasaur' || img.id == 'Ivysaur' || img.id == 'Venusaur'){
+                res.style.backgroundColor = 'forestgreen'
+                span.style.backgroundColor = 'rgb(3, 71, 3)'
+                span2.style.backgroundColor = 'rgb(3, 71, 3)'
+            }else{
+            if(img.id == 'Charmander' || img.id == 'Charmeleon' || img.id == 'Charizard'){
+                res.style.backgroundColor = 'red'
+                span.style.backgroundColor = 'rgb(95, 4, 4)'
+                span2.style.display = 'none'
+            }else{
+                if(img.id == 'Squirtle'|| img.id == 'Wartortle'|| img.id =='Blastoise'){
+                    res.style.backgroundColor = 'skyblue'
+                    span.style.backgroundColor = 'blue'
+                    span2.style.display = 'none'
+                }
+            }
+        }
+
+        if(img.id == 'Bulbasaur' || img.id == 'Ivysaur' || img.id == 'Venusaur'|| img.id == 'Charizard'){
+            span2.innerHTML = types[c].tipo2
+            span2.style.display = 'block'
+            span2.style.backgroundColor = 'purple'
+        }
+        
+        if(img.id == 'Charizard'){
+            span2.style.backgroundColor = 'skyblue'
+        }
+
         res.appendChild(p)
         res.appendChild(imgtemp)
+        res.appendChild(span)
+        res.appendChild(span2)
+        
     }
     li.appendChild(a)
     div.appendChild(li)
